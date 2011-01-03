9b7366e73497f1056720d3bab9313c90006d419d
Timothy Appnel <tim@appnel.com>
2011-01-03 18:46:12 -0500

    Refactored byline field transformer. Seperate entry/author callbacks wasn't needed after all.

--- plugins/bylines/lib/Melody/Bylines.pm
+++ plugins/bylines/lib/Melody/Bylines.pm
@@ -4,11 +4,14 @@ use warnings;
 
 #--- transformer handlers
 
-sub add_entry_byline_field {
+sub add_byline_field {
     my ( $eh, $app, $param, $tmpl ) = @_;
     return unless $tmpl->isa('MT::Template');
     my $q = $app->can('query') ? $app->query : $app->param;
-    my $model = $q->param('_type') ? 'page' : 'entry'; # assuming mode == view
+    my $model
+      = $q->param('_type') eq 'author'
+      ? 'author'
+      : 'entry';    # too presumptuous? die instead?
     my $byline = '';
     if ( my $id = $q->param('id') ) {
         require MT::Util;
@@ -16,18 +19,30 @@ sub add_entry_byline_field {
         $byline = MT::Util::encode_html( $obj->byline )
           if $obj && $obj->byline;
     }
-    my $innerHTML
-      = qq{ <textarea name="byline" id="byline" class="full-width short" cols="" rows="" mt:watch-change="1">$byline</textarea> };
-    my $host_node 
-      = $tmpl->getElementById('tags-field')
-      or $tmpl->getElementById('text-field')
-      or return $app->error('getElementById failed');
+    my $innerHTML;
+    my $host_node;
+    if ( $model eq 'author' ) {
+        $innerHTML = qq{ 
+    	<div class="textarea-wrapper">
+      		<textarea name="byline" id="byline" class="full-width" mt:watch-change="1">$byline</textarea>
+      	</div>	
+      };
+        $host_node = $tmpl->getElementById('url-field');
+    }
+    else {
+        $innerHTML
+          = qq{ <textarea name="byline" id="byline" class="full-width short" cols="" rows="" mt:watch-change="1">$byline</textarea> };
+        $host_node = $tmpl->getElementById('tags-field')
+          || $tmpl->getElementById('text-field');
+    }
+    return $app->error('getElementById failed') unless $host_node;
     my $block_node = $tmpl->createElement( 'app:setting',
-                                      { id => 'byline', label => 'byline' } );
+                                       { id => 'byline', label => 'byline' } )
+      ;    # need hint
     $block_node->innerHTML($innerHTML);
-    return $tmpl->insertBefore( $block_node, $host_node )
+    return $tmpl->insertAfter( $block_node, $host_node )
       or $app->error('failed to insertBefore');
-} ## end sub add_entry_byline_field
+} ## end sub add_byline_field
 
 sub save_byline {
     my ( $eh, $app, $obj, $orig ) = @_;
